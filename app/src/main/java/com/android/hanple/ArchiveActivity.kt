4c6cef73b6394a14161b59f4af9383151f9708d7
Sooyoung <agriades23@gmail.com>
2024-05-28 16:49:25 +0900

    feat: 검색 기록 및 보관함 페이지에 같이 사용하는 아카이브 액티비티 생성.

---
+++ app/src/main/java/com/android/hanple/ArchiveActivity.kt
@@ -0,0 +1,31 @@
+package com.android.hanple
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.android.hanple.databinding.ActivityArchiveBinding
+import com.android.hanple.databinding.ActivityOnboardingBinding
+
+class ArchiveActivity : AppCompatActivity() { //검색 기록 또는 보관함을 보여줍니다.
+    //검색 기록 및 보관함을 하단 탭으로 옮기고, 로그아웃 또는 탈퇴를 멀리 빼는 것은 어떨지...
+    //디자인 피드백 때 물어봐야겠음. 우선은 Activity로 작성하겠습니다.
+    private lateinit var binding: ActivityArchiveBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        binding = ActivityArchiveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        } enableEdgeToEdge()부터 화면비 조절 코드라고 설명 받음! 그렇구나.
+
+        val intent: Intent = intent //SettingsFragment에서 보낸 인텐트.
+        val testText = intent.getStringExtra("key")
+        binding.tvTest.text = testText
+    }
+}
