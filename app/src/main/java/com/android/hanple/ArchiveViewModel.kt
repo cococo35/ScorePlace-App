dcbf58c35a2478d0fb262ac6d586a9a4843bb827
Sooyoung <agriades23@gmail.com>
2024-05-28 17:02:38 +0900

    feat: ArchiveViewModel.kt 생성

---
+++ app/src/main/java/com/android/hanple/ArchiveViewModel.kt
@@ -0,0 +1,13 @@
+package com.android.hanple
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ArchiveViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "여기에 테스트 문구 전달"
+    }
+    val text: LiveData<String> = _text
+}
