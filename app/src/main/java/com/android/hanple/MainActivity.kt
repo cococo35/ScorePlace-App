4709ea3d8a7e2184160778a61eeeb880ab6bbc87
Sooyoung <agriades23@gmail.com>
2024-05-28 14:45:45 +0900

    feat: 메인 액티비티에 네비게이션을 붙임. (항목 1개 = 설정) activity_main.xml = fragment 자리 및 bottom navigation view 추가 MainActivity.kt = 바인딩 및 navigation 추가 (탭은 추후 추가)

--- app/src/main/java/com/android/hanple/MainActivity.kt
+++ app/src/main/java/com/android/hanple/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.android.hanple
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.android.hanple.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_fragment_host)
+
+//        각 메뉴 탭의 id를 setOf 안에 작성
+//        val appBarConfiguration = AppBarConfiguration(setOf(..., R.id.navigation_settings))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
     }
 }
+
