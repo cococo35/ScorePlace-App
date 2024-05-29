9aee370be6d1bc976dde4c63e02552ec62de5519
Sooyoung <agriades23@gmail.com>
2024-05-29 21:11:18 +0900

    refactor: feat/SharedPreference -> develop 머지 후 코드 충돌 수정

--- app/src/main/java/com/android/hanple/ui/SignUpActivity.kt
+++ app/src/main/java/com/android/hanple/ui/SignUpActivity.kt
@@ -1,9 +1,5 @@
 package com.android.hanple.ui
 
-import android.content.SharedPreferences
-import android.os.Bundle
-import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -37,26 +33,26 @@ class SignupActivity : AppCompatActivity() {
 // 여기서부터 feat/SharedPreferences 코드입니다.
         // DataStore 사용 전 SharedPreferences로 간단히 유저 id, pw 로컬 저장을 테스트함.
         // 참고한 자료: https://velog.io/@dlrmwl15/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-SharedPreferences
-        binding.btnSignup.setOnClickListener { // 회원가입 버튼 누를 시 id, pw 칸 정보 저장
-            val etId = binding.etId.text.toString()
-            val etPw = binding.etPassword.text.toString()
 
-            val sharedPreference = getSharedPreferences("userInfo", MODE_PRIVATE)
-            val editor: SharedPreferences.Editor = sharedPreference.edit()
-            editor.putString("userId", etId)
-            editor.putString("userPw", etPw)
-            editor.apply()
-        }
+//        binding.btnSignup.setOnClickListener { // 회원가입 버튼 누를 시 id, pw 칸 정보 저장 -> ViewModel과 연겵된 클릭 리스너 코드와 충돌할 수 있어서 주석 처리함.
+//            val etId = binding.etId.text.toString()
+//            val etPw = binding.etPassword.text.toString()
+//
+//            val sharedPreference = getSharedPreferences("userInfo", MODE_PRIVATE)
+//            val editor: SharedPreferences.Editor = sharedPreference.edit()
+//            editor.putString("userId", etId)
+//            editor.putString("userPw", etPw)
+//            editor.apply()
+//        }
 
-        binding.btnLogin.setOnClickListener {// 저장된 id, pw 로그에 띄워줌
-            val sharedPreference = getSharedPreferences("userInfo", MODE_PRIVATE)
-            val savedUserId = sharedPreference.getString("userId", "")
-            val savedUserPw = sharedPreference.getString("userPw", "")
-            Log.d("유저 정보", "저장된 ID: " + savedUserId)
-            Log.d("유저 정보", "저장된 PW: " + savedUserPw)
-        }
-    }
-}
+//        binding.btnLogin.setOnClickListener {// 저장된 id, pw 로그에 띄워줌 -> btnLogin 타 화면으로 이동해 주석 처리함.
+//            val sharedPreference = getSharedPreferences("userInfo", MODE_PRIVATE)
+//            val savedUserId = sharedPreference.getString("userId", "")
+//            val savedUserPw = sharedPreference.getString("userPw", "")
+//            Log.d("유저 정보", "저장된 ID: " + savedUserId)
+//            Log.d("유저 정보", "저장된 PW: " + savedUserPw)
+//        }
+
 // 여기까지 feat/SharedPreferences 코드입니다.
 
         binding.btnSignup.setOnClickListener {
