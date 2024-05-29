9217986ab9f5eb3c379ebaa9fdcec33e728121cc
seotoung19920921 <meba22@naver.com>
2024-05-29 14:52:11 +0900

    add : SignUp

---
+++ app/src/main/java/com/android/hanple/ui/SignUpActivity.kt
@@ -0,0 +1,40 @@
+package com.android.hanple.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import com.android.hanple.R
+import com.android.hanple.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val etId = binding.etId
+        val etPassword = binding.etPassword
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    }
+
+        }
+    }
+}
