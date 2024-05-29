bfbb630d7bebf7866d633acf818a572a9e1fd616
seotoung19920921 <meba22@naver.com>
2024-05-29 15:56:50 +0900

    fix : title error

--- app/src/main/java/com/android/hanple/ui/LogInctivity.kt
+++ app/src/main/java/com/android/hanple/ui/LogInctivity.kt
@@ -1,21 +1,18 @@
 package com.android.hanple.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.ViewModel
-import com.android.hanple.R
+import com.android.hanple.databinding.ActivityLoginBinding
+
 import com.android.hanple.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class LogInctivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
