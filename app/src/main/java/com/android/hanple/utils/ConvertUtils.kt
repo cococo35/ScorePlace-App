b62427bb35067ffa0a1a5df9a87b0be549431de6
Sooyoung <agriades23@gmail.com>
2024-05-30 16:19:24 +0900

    docs: 파일명이랑 class 이름 대소문자가 달라 수정함.

--- app/src/main/java/com/android/hanple/utils/ConvertUtils.kt
+++ app/src/main/java/com/android/hanple/utils/ConvertUtils.kt
@@ -7,8 +7,12 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 
 object ConvertUtils {
-    @RequiresApi(Build.VERSION_CODES.O)
+
+    //minSdk = 24, targetSdk = 34
+    //참고한 자료: https://stackoverflow.com/questions/47250263/kotlin-convert-timestamp-to-datetime
+    @RequiresApi(Build.VERSION_CODES.O) //API level 26 이상
     fun unixTimeConverter(unixTime: Long): LocalDateTime = Instant.ofEpochSecond(unixTime)
             .atZone(ZoneId.systemDefault()) //안드로이드 OS가 확인한 현재 시간대.
             .toLocalDateTime()
+
 }
