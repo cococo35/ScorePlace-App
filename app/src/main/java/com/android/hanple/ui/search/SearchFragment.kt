6bf6a9c28b640c33221911d68a1861766fa6ad00
JinHyungJun <132827490+JinHyungJunHongik@users.noreply.github.com>
2024-05-31 16:31:50 +0900

    장소 자동완성 기능 테스트 및 map sdk 추가

--- app/src/main/java/com/android/hanple/ui/search/SearchFragment.kt
+++ app/src/main/java/com/android/hanple/ui/search/SearchFragment.kt
@@ -1,18 +1,62 @@
 package com.android.hanple.ui.search
 
+import android.app.Activity
+import android.content.ContentValues
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentTransaction
 import com.android.hanple.R
+import com.android.hanple.databinding.FragmentSearchBinding
+import com.android.hanple.ui.MainActivity
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 
 class SearchFragment : Fragment() {
-
+    private val binding by lazy {
+        FragmentSearchBinding.inflate(layoutInflater)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAutoComplete()
+    }
+
+
+    private fun initAutoComplete(){
+        val autocompleteFragment = childFragmentManager.findFragmentById(R.id.start_autocomplete) as? AutocompleteSupportFragment
+
+        autocompleteFragment?.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        autocompleteFragment?.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                Log.d("선택 장소", "Place: ${place.name}, ${place.id}, ${place.latLng}")
+            }
+            override fun onError(status: Status) {
+                Log.i(ContentValues.TAG, "An error occurred: $status")
+            }
+        })
+    }
+
+
 }
