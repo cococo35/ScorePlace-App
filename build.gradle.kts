2409904ae95cc7bcad3ef2f1920ee45c02414933
Sooyoung <agriades23@gmail.com>
2024-06-03 13:41:35 +0900

    docs: 모듈 Gradle 파일에 google-services 플러그인, Firebase SDK 추가 (Firebase SDK 사용을 위한 사전 작업 2, 끝)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 
-    //Firebase SDK 사용을 위한 사전 작업 1: Google services Gradle plugin 디펜던시 추가.
+    //Firebase SDK 사용을 위한 사전 작업 1: 프로젝트 수준의 build.gradle.kts 파일에 플러그인을 종속 항목으로 추가
     //참고: 현재 이 파일은 루트 수준(프로젝트 수준) Gradle 파일입니다. Hanple/build.gradle.kts
     id("com.google.gms.google-services") version "4.4.2" apply false
 
