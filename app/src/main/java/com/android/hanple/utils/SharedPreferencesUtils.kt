2dc68f8861a85c7429140b1b2ed6298420e088ab
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-04 18:26:37 +0900

    refactor: SharedPreferencesUtils 다른 Utils랑 이름 컨벤션 맞춰줌

--- app/src/main/java/com/android/hanple/utils/SharedPreferencesUtils.kt
+++ app/src/main/java/com/android/hanple/utils/SharedPreferencesUtils.kt
@@ -1,11 +1,10 @@
-package com.android.hanple.ui.onboarding
+package com.android.hanple.utils
 
 import android.content.SharedPreferences
 
-class SharedPreferences(spf: SharedPreferences) {
-    companion object {
-        val TAG = "LogInActivity, SharedPreference"
-    }
+object SharedPreferencesUtils { //우선은 Singleton으로 선언해 어디에서든 사용가능하게 두었습니다.
+    //추후, 아키텍처에 맞추어 메소드들을 옮겨주시면 됩니다.
+    //내부 함수들이 다 제자리를 찾으면 SharedPreferencesUtils는 사라져도 무방합니다.
 
     //SharedPreferences 읽고 쓰고 지우기 - https://hapen385.tistory.com/29 코드를 참고함.
     fun rememberMe(spf: SharedPreferences, email: String) { // sharedPreference에 이메일 저장.
@@ -13,10 +12,10 @@ class SharedPreferences(spf: SharedPreferences) {
         editor.putString("remember_me", email).apply()
     }
     //앱 실행 전에 SharedPreferences에 저장된 이메일 정보가 있다 = 로컬에 저장됨 = 자동 로그인.
-    private fun loadRememberMe(spf: SharedPreferences) =
+    fun loadRememberMe(spf: SharedPreferences) =
         spf.getString("remember_me", "") //초기값 null 아닌 ""
 
-    private fun deleteRememberMe(spf: SharedPreferences) {
+    fun deleteRememberMe(spf: SharedPreferences) {
         val editor = spf.edit()
         editor.remove("remember_me").apply()
     }
