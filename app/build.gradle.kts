952717e8050dd6e6abe1323081c2ea94bc94c3e9
seotoung19920921 <meba22@naver.com>
2024-06-05 12:20:15 +0900

    fix : API 등록 오류 고치는중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,8 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     //retrofit etc..
     implementation ("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("com.google.android.gms:play-services-maps:17.0.0")
+    implementation ("com.google.android.gms:play-services-location:17.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("io.coil-kt:coil:2.6.0")
--- app/src/main/java/com/android/hanple/ui/MapFragment.kt
+++ app/src/main/java/com/android/hanple/ui/MapFragment.kt
@@ -1,39 +1,97 @@
-package com.example.mapdemo
-
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.android.hanple.databinding.FragmentMapBinding
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import com.android.hanple.R
 
-class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var map: GoogleMap
+    companion object {
+        const val TAG = "MapFragment"
+    }
+
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+    private var currentMarker: Marker? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map_container) as SupportMapFragment?
-            ?: SupportMapFragment.newInstance().also {
-                childFragmentManager.beginTransaction()
-                    .replace(R.id.map_container, it)
-                    .commit()
-            }
-
-        mapFragment.getMapAsync(this)
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this@MapFragment)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        val lat = arguments?.getDouble("lat") ?: 0.0
-        val lng = arguments?.getDouble("lng") ?: 0.0
-        val location = LatLng(lat, lng)
-        map.addMarker(MarkerOptions().position(location).title("Selected Location"))
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+        this.googleMap = googleMap
+
+        currentMarker = setupMarker(LatLngEntity(37.5562, 126.9724))  // default 서울역
+        currentMarker?.showInfoWindow()
     }
+
+    private fun setupMarker(locationLatLngEntity: LatLngEntity): Marker? {
+        val positionLatLng = LatLng(locationLatLngEntity.latitude!!, locationLatLngEntity.longitude!!)
+        val markerOption = MarkerOptions().apply {
+            position(positionLatLng)
+            title("위치")
+            snippet("서울역 위치")
+        }
+
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL  // 지도 유형 설정
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(positionLatLng, 15f))  // 카메라 이동
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15f))  // 줌의 정도 - 1 일 경우 세계지도 수준, 숫자가 커질 수록 상세지도가 표시됨
+        return googleMap.addMarker(markerOption)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroyView() {
+        mapView.onDestroy()
+        super.onDestroyView()
+        _binding = null
+    }
+
+    data class LatLngEntity(
+        var latitude: Double?,
+        var longitude: Double?
+    )
 }
