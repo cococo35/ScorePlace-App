e075fdabe3413785a1dd84944e1f0cd56e0ced02
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-07 17:45:11 +0900

    feat: viewModel observer로 AuthState.Success 확인하면 메인으로 intent 이동

--- app/src/main/java/com/android/hanple/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/android/hanple/ui/onboarding/OnboardingActivity.kt
@@ -2,9 +2,12 @@ package com.android.hanple.ui.onboarding
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.android.hanple.databinding.ActivityOnboardingBinding
+import com.android.hanple.ui.MainActivity
 
 class OnboardingActivity : AppCompatActivity() {
 
@@ -19,6 +22,7 @@ class OnboardingActivity : AppCompatActivity() {
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
             authViewModel.logIn(email, password)
+            //email이나 password가 하나라도 null이면 현재 앱이 강제 종료됨.
         }
 
         // 회원가입 텍스트뷰 클릭 리스너 설정
@@ -26,5 +30,20 @@ class OnboardingActivity : AppCompatActivity() {
             val signupIntent = Intent(this, SignUpActivity::class.java)
             startActivity(signupIntent)
         }
+
+        authViewModel.authState.observe(this, Observer { authState ->
+            when (authState) {
+                is AuthViewModel.AuthState.Success -> {
+                    Toast.makeText(this, "Firebase Auth 인증 성공!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()  // 현재 Activity를 종료하여 뒤로가기 시 로그인 화면이 보이지 않도록 함
+                }
+                is AuthViewModel.AuthState.Failure -> {
+                    Toast.makeText(this, "Firebase Auth 인증 실패: ${authState.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
     }
 }
