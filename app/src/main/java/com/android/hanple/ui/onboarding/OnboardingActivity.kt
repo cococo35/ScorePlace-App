95661b3711803b0eb33c4e90473562ec042967d4
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-07 16:59:16 +0900

    refactor: 버튼 인텐트 수정

--- app/src/main/java/com/android/hanple/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/android/hanple/ui/onboarding/OnboardingActivity.kt
@@ -23,7 +23,7 @@ class OnboardingActivity : AppCompatActivity() {
 
         // 회원가입 텍스트뷰 클릭 리스너 설정
         binding.tvSignup.setOnClickListener {
-            val signupIntent = Intent(this, NewAccountActivity::class.java)
+            val signupIntent = Intent(this, SignUpActivity::class.java)
             startActivity(signupIntent)
         }
     }
