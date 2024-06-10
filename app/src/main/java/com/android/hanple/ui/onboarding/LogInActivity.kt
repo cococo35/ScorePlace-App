25b9720258e228706485fd1fec57932644d578dd
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-10 18:13:54 +0900

    docs: AuthViewModel -> LogInViewModel로 수정

--- app/src/main/java/com/android/hanple/ui/onboarding/LogInActivity.kt
+++ app/src/main/java/com/android/hanple/ui/onboarding/LogInActivity.kt
@@ -15,7 +15,7 @@ import com.android.hanple.utils.SharedPreferencesUtils
 class LogInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogInBinding
-    private val authViewModel: AuthViewModel by viewModels()
+    private val logInViewModel: LogInViewModel by viewModels()
 
     private fun initView(){
         if (binding.cbAutoLogin.isChecked) {
@@ -36,7 +36,7 @@ class LogInActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {//앱 자체 null 체크
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
-            val logInChecker: Int = authViewModel.logIn(email, password) //여기서 로그인!
+            val logInChecker: Int = logInViewModel.logIn(email, password) //여기서 로그인!
             when(logInChecker) {
                 1 -> Toast.makeText(this, "이메일 란이 비어있어요.", Toast.LENGTH_SHORT).show()
                 2 -> Toast.makeText(this, "비밀번호 란이 비어있어요.", Toast.LENGTH_SHORT).show()
@@ -51,15 +51,15 @@ class LogInActivity : AppCompatActivity() {
             startActivity(signupIntent)
         }
 
-        authViewModel.authState.observe(this, Observer { authState -> //firebase auth 계정 체크
+        logInViewModel.authState.observe(this, Observer { authState -> //firebase auth 계정 체크
             when (authState) {
-                is AuthViewModel.AuthState.Success -> {
+                is LogInViewModel.AuthState.Success -> {
                     Toast.makeText(this, "Firebase Auth 인증 성공!", Toast.LENGTH_SHORT).show()
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     finish()  // 현재 Activity를 종료하여 뒤로가기 시 로그인 화면이 보이지 않도록 함
                 }
-                is AuthViewModel.AuthState.Failure -> {
+                is LogInViewModel.AuthState.Failure -> {
                     val exception: String = "${authState.exception?.message}"
                     var toastMessage: String = ""
                     Log.d("Firebase Auth Failed", exception)
