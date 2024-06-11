f22a98b7a64e1defda4d63ecbecc47a861d9a935
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-11 16:11:24 +0900

    refactor: 뒤로가기 버튼 주석 다시 활성화

--- app/src/main/java/com/android/hanple/ui/onboarding/SignUpActivity.kt
+++ app/src/main/java/com/android/hanple/ui/onboarding/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.android.hanple.ui.onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -38,12 +39,12 @@ class SignUpActivity : AppCompatActivity() {
 ////            }
 //        }
 
-//        // 뒤로 가기 아이콘 클릭 리스너 설정
-//        binding.ivBack.setOnClickListener {
-//            val loginIntent = Intent(this, LogInActivity::class.java)
-//            startActivity(loginIntent)
-//            finish() // 현재 액티비티를 종료
-//        }
+        // 뒤로 가기 아이콘 클릭 리스너 설정
+        binding.ivBack.setOnClickListener {
+            val loginIntent = Intent(this, LogInActivity::class.java)
+            startActivity(loginIntent)
+            finish() // 현재 액티비티를 종료
+        }
 
 //        binding.btnSignup.setOnClickListener {
 ////            handleSignup(editor, sharedPreference)
