41c9bc2fde5ab5e4c0845c71e3b27bc9017da259
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-13 19:22:05 +0900

    feat: guest uid sharedPref에 저장, 불러오기, 삭제하는 메소드 추가

--- app/src/main/java/com/android/hanple/utils/SharedPreferencesUtils.kt
+++ app/src/main/java/com/android/hanple/utils/SharedPreferencesUtils.kt
@@ -5,26 +5,46 @@ import android.content.SharedPreferences
 
 //context 부족해 생긴 오류 디버깅을 했다.
 //참고한 글: https://youngdroidstudy.tistory.com/entry/Kotlin-%EC%BD%94%ED%8B%80%EB%A6%B0%EC%9D%98-SharedPreferences-%EB%9E%80
-class SharedPreferencesUtils(context: Context) { //context 매개변수를 위해 Singleton에서 class로 다시 변경함. 대신 사용하는 Activity에서 companion object로 불러오겠습니다.
-    //추후, 아키텍처에 맞추어 메소드들을 옮겨주시면 됩니다.
-    //내부 함수들이 다 제자리를 찾으면 SharedPreferencesUtils는 사라져도 무방합니다.
+class SharedPreferencesUtils(context: Context) {
+
+    companion object { //참고한 자료: https://tahaben.com.ly/2023/02/using-sharedpreferences-to-save-basic-user-info/
+        private const val FIREBASE_AUTH_LOCAL = "firebase_auth"
+        private const val FIREBASE_EMAIL = "email"
+        private const val FIREBASE_GUEST_UID = "guest_uid"
+    }
 
     //SharedPreferences 읽고 쓰고 지우기 - https://hapen385.tistory.com/29 코드를 참고함.
     private val spf: SharedPreferences =
-        context.getSharedPreferences("remember_me", Context.MODE_PRIVATE)
+        context.getSharedPreferences(FIREBASE_AUTH_LOCAL, Context.MODE_PRIVATE)
 
-    fun rememberMe(email: String) { // sharedPreference에 이메일 저장.
-        val editor: SharedPreferences.Editor = spf.edit()
-        editor.putString("remember_me", email).apply()
+
+    //게스트 로그인 정보: uid
+    fun saveGuestUidLocal(uid: String) {
+        spf.edit()
+            .putString(FIREBASE_GUEST_UID, uid)
+            .apply()
+    }
+    fun loadGuestUidLocal(): String? {
+        return spf.getString(FIREBASE_GUEST_UID, null)
+    }
+    fun deleteGuestUidLocal() {
+        spf.edit()
+            .remove(FIREBASE_EMAIL)
+            .apply()
     }
 
-    //앱 실행 전에 SharedPreferences에 저장된 이메일 정보가 있다 = 로컬에 저장됨 = 자동 로그인.
-    fun loadRememberMe(): String =
-        spf.getString("remember_me", "").toString() //초기값 null 아닌 "" 일 것으로 예상 중.
-    //defValue를 지정하는데도. spf.getStirng 타입이 String? 이라서 좀 놀랐어요.
-
-    fun deleteRememberMe() {
-        val editor = spf.edit()
-        editor.remove("remember_me").apply()
+    //이메일
+    fun saveEmailLocal(email: String) {
+        spf.edit()
+            .putString(FIREBASE_EMAIL, email)
+            .apply()
+    }
+    fun loadEmailLocal(): String? {
+        return spf.getString(FIREBASE_EMAIL, null)
+    }
+    fun deleteEmailLocal() {
+        spf.edit()
+            .remove(FIREBASE_EMAIL)
+            .apply()
     }
 }
