de156c5e5b8c03ebf41b5c7ae8f7b33dbf546406
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-18 17:55:23 +0900

    feat: 비밀번호, 이메일 유효성 검사

--- app/src/main/java/com/android/hanple/ui/test/LocalSignUpActivity.kt
+++ app/src/main/java/com/android/hanple/ui/test/LocalSignUpActivity.kt
@@ -1,8 +1,6 @@
 package com.android.hanple.ui.test
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -18,24 +16,27 @@ class LocalSignUpActivity : AppCompatActivity() {
         binding = ActivityLocalSignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.etPassword.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun afterTextChanged(editable: Editable?) {
-                viewModel.setPassword(editable.toString()) //viewModel.setPassword 메소드에 매개변수로 etPassword text 넣기
-            }
-        })
-
-        // 비밀번호 유효성 상태 관찰
-        lifecycleScope.launch {
-            viewModel.isPasswordValid.collect { isValid ->
-                binding.tvError.text = if (isValid) "비밀번호 8자 이상 통과" else ""
+        // EditText 포커스 변경 리스너 설정
+        binding.etPassword.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                lifecycleScope.launch {
+                    viewModel.isPasswordValid.collect { isValid ->
+                        binding.tvError.text =
+                            if (isValid) "비밀번호 8자 이상 통과 >.<" else "비밀번호가 8자 미만이에요;;;"
+                    }
+                }
             }
         }
+
+        binding.etEmail.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                lifecycleScope.launch {
+                    viewModel.isEmailValid.collect { isValid ->
+                        binding.tvError.text =
+                            if (isValid) "이메일 주소 형식이 맞네요!" else "잘못된 이메일 형식이에요 ㅠ.ㅠ"
+                    }
+                }
+            }
+        }1
     }
 }
--- app/src/main/java/com/android/hanple/ui/test/LocalSignUpViewModel.kt
+++ app/src/main/java/com/android/hanple/ui/test/LocalSignUpViewModel.kt
@@ -12,9 +12,22 @@ class LocalSignUpViewModel: ViewModel() {
     private val _isPasswordValid = MutableStateFlow(false)
     val isPasswordValid: StateFlow<Boolean> = _isPasswordValid
 
+    private val _email: MutableStateFlow<String> = MutableStateFlow("")
+    val email: StateFlow<String> = _email
+
+    private val _isEmailValid = MutableStateFlow(false)
+    val isEmailValid: StateFlow<Boolean> = _isEmailValid
+
     fun setPassword(password: String) {
         _password.value = password
         _isPasswordValid.value = password.length >= 8
+    }
+
+    fun setEmail(email: String) {
+        _email.value = email
+        _isEmailValid.value = android.util.Patterns
+            .EMAIL_ADDRESS.matcher(email).matches()
+        //android.util.Patterns.EMAIL_ADDRESS로 이메일 형식 확인 가능. 안드로이드 내장값
     }
 
 }
