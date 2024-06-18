fa4ad5a777b0c6ee262d6420b01f14616404cf87
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-18 16:42:14 +0900

    docs: 매니페스트 수정, 비밀번호 8자리 이상이면 에러 메시지 변화하도록 ViewModel에 StateFlow 작성

--- app/src/main/java/com/android/hanple/ui/test/LocalSignUpActivity.kt
+++ app/src/main/java/com/android/hanple/ui/test/LocalSignUpActivity.kt
@@ -3,7 +3,10 @@ package com.android.hanple.ui.test
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.android.hanple.databinding.ActivityLocalSignUpBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class LocalSignUpActivity : AppCompatActivity() {
 
@@ -15,6 +18,14 @@ class LocalSignUpActivity : AppCompatActivity() {
         binding = ActivityLocalSignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.
-    }
-}
+        lifecycleScope.launch {
+            viewModel.isPasswordValid.collectLatest { isValid ->
+                if (isValid) {
+                    binding.tvError.text = "비밀번호 조건 만족"
+                } else {
+                    binding.tvError.text = "비밀번호가 8자리 미만입니다."
+                }
+            }
+        }
+    } //onCreate 끝.
+} //Activity 끝.
--- app/src/main/java/com/android/hanple/ui/test/LocalSignUpViewModel.kt
+++ app/src/main/java/com/android/hanple/ui/test/LocalSignUpViewModel.kt
@@ -2,15 +2,23 @@ package com.android.hanple.ui.test
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class LocalSignUpViewModel : ViewModel() {
 
     //StateFlow
     private val _password: MutableStateFlow<String> = MutableStateFlow("")
-    val password: MutableStateFlow<String> = _password
+    val password: StateFlow<String> = _password
 
-    fun setPassword(password: String) {
-        _password.value = password
+    private val _isPasswordValid = MutableStateFlow(false)
+    val isPasswordValid: StateFlow<Boolean> = _isPasswordValid
+
+    fun setPassword(newPassword: String) {
+        _password.value = newPassword
+        _isPasswordValid.value = isValidPassword(newPassword)
     }
 
+    private fun isValidPassword(password: String): Boolean {
+        return password.length >= 8
+    }
 }
