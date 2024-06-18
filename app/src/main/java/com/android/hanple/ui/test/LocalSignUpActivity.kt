6f95a0c19cc11be6be3aab823d3e0a52bca551ba
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-18 18:10:20 +0900

    feat: 비밀번호 8자 이상이면 글자 색상도 변하는 기능 추가.

--- app/src/main/java/com/android/hanple/ui/test/LocalSignUpActivity.kt
+++ app/src/main/java/com/android/hanple/ui/test/LocalSignUpActivity.kt
@@ -1,9 +1,13 @@
 package com.android.hanple.ui.test
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.android.hanple.R
 import com.android.hanple.databinding.ActivityLocalSignUpBinding
 import kotlinx.coroutines.launch
 
@@ -16,13 +20,40 @@ class LocalSignUpActivity : AppCompatActivity() {
         binding = ActivityLocalSignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // etPassword 텍스트 변경 리스너 추가
+        binding.etPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(editable: Editable?) {
+                viewModel.setPassword(editable.toString())
+            }
+        })
+
+        // etEmail 텍스트 변경 리스너 추가
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(editable: Editable?) {
+                viewModel.setEmail(editable.toString())
+            }
+        })
+
         // EditText 포커스 변경 리스너 설정
         binding.etPassword.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
                 lifecycleScope.launch {
                     viewModel.isPasswordValid.collect { isValid ->
-                        binding.tvError.text =
-                            if (isValid) "비밀번호 8자 이상 통과 >.<" else "비밀번호가 8자 미만이에요;;;"
+                        if (isValid) {
+                            binding.tvError.text = "비밀번호 8자 이상 통과 >.<"
+                            binding.tvError.setTextColor(
+                                ContextCompat.getColor( this@LocalSignUpActivity, R.color.darkblue)
+                            )
+                        } else {
+                            binding.tvError.text = "비밀번호가 8자 미만이에요;;;"
+                            binding.tvError.setTextColor(
+                                ContextCompat.getColor(this@LocalSignUpActivity,R.color.darkmint2)
+                                )
+                        }
                     }
                 }
             }
@@ -37,6 +68,6 @@ class LocalSignUpActivity : AppCompatActivity() {
                     }
                 }
             }
-        }1
+        }
     }
 }
