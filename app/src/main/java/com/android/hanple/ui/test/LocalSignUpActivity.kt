9e7697dfaacd2251b75457b4ccaa7965652040be
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-18 16:52:49 +0900

    refactor: 뷰바인딩

--- app/src/main/java/com/android/hanple/ui/test/LocalSignUpActivity.kt
+++ app/src/main/java/com/android/hanple/ui/test/LocalSignUpActivity.kt
@@ -1,21 +1,16 @@
 package com.android.hanple.ui.test
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.android.hanple.R
+import com.android.hanple.databinding.ActivityLocalSignUpBinding
 
 class LocalSignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLocalSignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_local_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityLocalSignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
