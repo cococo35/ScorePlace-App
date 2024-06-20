5069aad60936bdba8d78561651e9731e6971ed3f
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-20 10:56:26 +0900

    feat: 스크롤 밑으로 내려오면 체크되도록. 현재 문제: 버튼 색은 바뀌는데 버튼 내 글자도 색깔이랑 똑같이 바뀌게 하고 싶음. 색상은 selector 사용했지만 글자는 kotlin에서 처리해야해 까다롭다.

--- app/src/main/java/com/android/hanple/ui/onboarding/PrivatePolicyActivity.kt
+++ app/src/main/java/com/android/hanple/ui/onboarding/PrivatePolicyActivity.kt
@@ -3,6 +3,7 @@ package com.android.hanple.ui.onboarding
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.android.hanple.R
 import com.android.hanple.databinding.ActivityPrivatePolicyBinding
 
 class PrivatePolicyActivity : AppCompatActivity() {
@@ -15,12 +16,18 @@ class PrivatePolicyActivity : AppCompatActivity() {
         binding = ActivityPrivatePolicyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.cbReadAll.setOnCheckedChangeListener { _, isChecked -> //ViewModel에 체크박스 체크 여부 전송
-            viewModel.updateReadAll(isChecked)
+         binding.sv.setOnScrollChangeListener { _, _, _, _, _ ->
+            val isAtBottom: Boolean
+                    = binding.sv.scrollY + binding.sv.height >= binding.sv.getChildAt(0).measuredHeight
+                viewModel.updateReadAll(isAtBottom) // 뷰모델로 스크롤 다 내렸는지 아닌지 체크
         }
+
         viewModel.readAll.observe(this) { isChecked -> //ViewModel에서
             binding.btnDismiss.isEnabled = isChecked
+        }
+
+        viewModel.buttonText.observe(this) { text -> //ViewModel에서
+            binding.btnDismiss.isEnabled = text
         }
 
         binding.btnDismiss.setOnClickListener {
