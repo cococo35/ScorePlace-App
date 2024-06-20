deaa7308b14e843e2dfca2a0c4bebfe1b1f1af58
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-20 14:37:48 +0900

    refactor: 스크롤이 끝까지 내려간 상태여야만 약관 동의 가능하게 변경. (다음주 디자인 피드백때 다시 올라가도 버튼 활성화할지 말지 물어보기)

--- app/src/main/java/com/scoreplace/hanple/ui/onboarding/PrivatePolicyActivity.kt
+++ app/src/main/java/com/scoreplace/hanple/ui/onboarding/PrivatePolicyActivity.kt
@@ -3,6 +3,7 @@ package com.scoreplace.hanple.ui.onboarding
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.scoreplace.hanple.R
 import com.scoreplace.hanple.databinding.ActivityPrivatePolicyBinding
 
 class PrivatePolicyActivity : AppCompatActivity() {
@@ -15,16 +16,32 @@ class PrivatePolicyActivity : AppCompatActivity() {
         binding = ActivityPrivatePolicyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //View -> ViewModel로 정보 전송
+        binding.sv.setOnScrollChangeListener { _, _, _, _, _ ->
+            val isAtBottom: Boolean
+                    = binding.sv.scrollY + binding.sv.height >= binding.sv.getChildAt(0).measuredHeight
+            viewModel.updateReadAll(isAtBottom) // 뷰모델로 스크롤 다 내렸는지 아닌지 체크
+        }
 
-//        binding.cbReadAll.setOnCheckedChangeListener { _, isChecked -> //ViewModel에 체크박스 체크 여부 전송
-//            viewModel.updateReadAll(isChecked)
-//        }
+        //ViewModel -> View 정보 받아오기
         viewModel.readAll.observe(this) { isChecked -> //ViewModel에서
             binding.btnDismiss.isEnabled = isChecked
+            changeButtonText(isChecked)
         }
 
+        //View 내 처리사항
+        binding.ivBack.setOnClickListener {
+            finish()
+        } //그냥 뒤로 가기
         binding.btnDismiss.setOnClickListener {
             finish()
-        }
+        } //체크 후 뒤로 가기
+
+    }
+
+    private fun changeButtonText(isChecked: Boolean) { //텍스트는 color와 달리 selector 없어 여기 작성함.
+        if (isChecked) {
+            binding.btnDismiss.text = getString(R.string.privacy_policy_scrolled)
+        } else binding.btnDismiss.text = getString(R.string.privacy_policy_unscrolled)
     }
 }
