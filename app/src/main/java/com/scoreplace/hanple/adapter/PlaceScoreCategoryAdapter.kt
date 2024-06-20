05c12d6866e7ab4970d2e7ecab291bbe2ec6175a
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-20 12:01:50 +0900

    refactor: 모든 com.android.hanple -> com.scoreplace.hanple로 바꿈. 남은 일: firebase 콘솔 상에서 패키지명 변경

---
+++ app/src/main/java/com/scoreplace/hanple/adapter/PlaceScoreCategoryAdapter.kt
@@ -0,0 +1,84 @@
+package com.scoreplace.hanple.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.scoreplace.hanple.data.CategoryPlace
+import com.scoreplace.hanple.databinding.RecyclerviewScoreCategoryItemBinding
+import com.bumptech.glide.Glide
+
+
+
+interface OnDataClick {
+    fun onItemClick(data: CategoryPlace)
+
+}
+class PlaceScoreCategoryAdapter(
+    private val onDataClick: com.scoreplace.hanple.adapter.OnDataClick
+) : ListAdapter<CategoryPlace, com.scoreplace.hanple.adapter.PlaceScoreCategoryAdapter.PlaceViewHolder>(
+    _root_ide_package_.com.scoreplace.hanple.adapter.PlaceScoreCategoryAdapter.Companion.diffCallback
+) {
+
+    inner class PlaceViewHolder(
+        private val binding: RecyclerviewScoreCategoryItemBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(place: CategoryPlace) = with(binding) {
+            binding.tvItemAddress.text = place.name
+            if(place.score == null){
+                binding.tvItemGrade.text = "점수 없음"
+            }
+            else {
+                binding.tvItemGrade.text = place.score.toString()
+            }
+            binding.tvItemTime.text = place.openingHours?.hoursType?.name
+            binding.root.setOnClickListener {
+                onDataClick.onItemClick(place)
+            }
+                if (place.img != null) {
+                    binding.ivItemThumbnail.visibility = View.VISIBLE
+                    binding.tvItemNullImage.visibility = View.GONE
+                    Glide.with(itemView).load(place.img).into(binding.ivItemThumbnail)
+                } else {
+                    binding.ivItemThumbnail.visibility = View.INVISIBLE
+                    binding.tvItemNullImage.visibility = View.VISIBLE
+                }
+            }
+        
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): _root_ide_package_.com.scoreplace.hanple.adapter.PlaceScoreCategoryAdapter.PlaceViewHolder {
+        val view = RecyclerviewScoreCategoryItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return PlaceViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: _root_ide_package_.com.scoreplace.hanple.adapter.PlaceScoreCategoryAdapter.PlaceViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    companion object {
+        val diffCallback = object : DiffUtil.ItemCallback<CategoryPlace>() {
+            override fun areItemsTheSame(
+                oldItem: CategoryPlace,
+                newItem: CategoryPlace
+            ): Boolean {
+               return oldItem.id == newItem.id
+            }
+            @SuppressLint("DiffUtilEquals")
+            override fun areContentsTheSame(
+                oldItem: CategoryPlace,
+                newItem: CategoryPlace
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
