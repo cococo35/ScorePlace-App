5b689dad3de424da3dcea1a033dde20f0a0f3c83
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-21 16:14:06 +0900

    refactor: 액티비티로 롤백 후 다시 구조 짜기

--- app/src/main/java/com/scoreplace/hanple/ui/onboard/OnboardActivity.kt
+++ app/src/main/java/com/scoreplace/hanple/ui/onboard/OnboardActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.scoreplace.hanple.R
 
 class OnboardActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
