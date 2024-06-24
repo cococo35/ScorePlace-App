a3bcc67b2b757eef0c6920e40245aeec8ea67f72
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-24 15:26:47 +0900

    refactor: 스크롤 수정 중

--- app/src/main/java/com/scoreplace/hanple/ui/onboarding/PrivatePolicyActivity.kt
+++ app/src/main/java/com/scoreplace/hanple/ui/onboarding/PrivatePolicyActivity.kt
@@ -1,8 +1,14 @@
 package com.scoreplace.hanple.ui.onboarding
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.scoreplace.hanple.R
 import com.scoreplace.hanple.databinding.ActivityPrivatePolicyBinding
 
@@ -17,16 +23,74 @@ class PrivatePolicyActivity : AppCompatActivity() {
         binding = ActivityPrivatePolicyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //View -> ViewModel로 정보 전송
+        binding.sv.setOnScrollChangeListener { _, _, _, _, _ ->
+            val isAtBottom: Boolean
+                    = binding.sv.scrollY + binding.sv.height >= binding.sv.getChildAt(0).measuredHeight
+            viewModel.updateReadAll(isAtBottom) // 뷰모델로 스크롤 다 내렸는지 아닌지 체크
+        }
 
-        binding.cbReadAll.setOnCheckedChangeListener { _, isChecked -> //ViewModel에 체크박스 체크 여부 전송
-            viewModel.updateReadAll(isChecked)
-        }
-        viewModel.readAll.observe(this) { isChecked -> //ViewModel에서
-            binding.btnDismiss.isEnabled = isChecked
+        //ViewModel -> View 정보 받아오기
+        viewModel.scrolled.observe(viewLifecycleOwner) { isChecked -> //ViewModel에서
+            binding.btnYes.isEnabled = isChecked
+            changeButtonText(isChecked)
         }
 
-        binding.btnDismiss.setOnClickListener {
-            finish()
-        }
+        binding.btnYes.setOnClickListener{
+            viewModel.updateAgreeOnPrivacyPolicy(true)
+            findNavController().navigateUp() }
+    }
+        private fun changeButtonText(isChecked: Boolean) { //텍스트는 color와 달리 selector 없어 여기 작성함.
+        if (isChecked) {
+            binding.btnYes.text = getString(R.string.privacy_policy_scrolled)
+        } else binding.btnYes.text = getString(R.string.privacy_policy_unscrolled)
     }
 }
+
+
+//class PrivacyPolicyFragment : Fragment() {
+//
+//    private var _binding: FragmentPrivacyPolicyBinding? = null
+//    private val binding get() = _binding!!
+//    private val viewModel: OnboardViewModel by viewModels()
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        _binding = FragmentPrivacyPolicyBinding.inflate(inflater, container, false)
+//
+//        //View -> ViewModel로 정보 전송
+//        binding.sv.setOnScrollChangeListener { _, _, _, _, _ ->
+//            val isAtBottom: Boolean
+//                    = binding.sv.scrollY + binding.sv.height >= binding.sv.getChildAt(0).measuredHeight
+//            viewModel.updateReadAll(isAtBottom) // 뷰모델로 스크롤 다 내렸는지 아닌지 체크
+//        }
+//
+//        //ViewModel -> View 정보 받아오기
+//        viewModel.scrolled.observe(viewLifecycleOwner) { isChecked -> //ViewModel에서
+//            binding.btnYes.isEnabled = isChecked
+//            changeButtonText(isChecked)
+//        }
+//
+//        binding.ivBack.setOnClickListener{
+//            viewModel.updateAgreeOnPrivacyPolicy(false)
+//            findNavController().navigateUp() }
+//        binding.btnYes.setOnClickListener{
+//            viewModel.updateAgreeOnPrivacyPolicy(true)
+//            findNavController().navigateUp() }
+//        return binding.root
+//    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//    }
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+//
+//    private fun changeButtonText(isChecked: Boolean) { //텍스트는 color와 달리 selector 없어 여기 작성함.
+//        if (isChecked) {
+//            binding.btnYes.text = getString(R.string.privacy_policy_scrolled)
+//        } else binding.btnYes.text = getString(R.string.privacy_policy_unscrolled)
+//    }
