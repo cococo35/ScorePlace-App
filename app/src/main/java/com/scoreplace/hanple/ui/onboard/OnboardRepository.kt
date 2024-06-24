d94ad1326ff93a3b87378d37a5417e5f467a0c3f
SooyoungHwang <75528131+agriades@users.noreply.github.com>
2024-06-24 15:19:04 +0900

    feat: 빈 레포지토리 생성

---
+++ app/src/main/java/com/scoreplace/hanple/ui/onboard/OnboardRepository.kt
@@ -0,0 +1,12 @@
+package com.scoreplace.hanple.ui.onboard
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class OnboardRepository {
+
+    private val auth = Firebase.auth
+    private val db = Firebase.firestore
+
+}
